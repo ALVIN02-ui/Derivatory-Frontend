7a1b870f9c6584403b5d623226a4c2c8d8d8b17a
Tyrell Ondijo <tyrellondijo@yahoo.com>
2023-11-04 01:37:33 +0300

    Styled most of the sign up page

--- src/pages/Others/Login.jsx
+++ src/pages/Others/Login.jsx
@@ -19,28 +19,29 @@ const Login = () => {
   const { open, currentMode} = useStateContext();
   return (
     <div className={`${open ? "pl-72" : "pl-20"} ${currentMode === 'Dark' ? 'dark bg-slate-900 text-white': 'bg-slate-200 text-gray-800'} h-screen w-screen`}>
-      <div className="signup-container">
 
-        <div className="header">
-          <div className="text">Sign Up</div>
-          <div className="underline"></div>
+      <div className={`signup-container flex flex-col m-auto mt-20 w-2/5 ${currentMode === 'Dark' ? 'dark bg-slate-800 text-white': 'bg-slate-100 text-gray-800'}`}>
+
+        <div className="header flex flex-col items-center gap-1.5 w-full mt-8">
+          <div className="text text-2xl font-medium">Sign Up</div>
+          <div className={`underline w-12 h-1 rounded-md ${currentMode === 'Dark' ? 'dark bg-white': 'bg-slate-800'}`}></div>
         </div>
 
-        <div className="inputs">
+        <div className="inputs mt-10 flex flex-col gap-6">
 
           <div className="input">
             <img src={user_icon} alt="" />
-            <input type="text" />
+            <input type="text" placeholder='Username'/>
           </div>
 
           <div className="input">
-            <img src={email_icon} alt="" />
-            <input type="email" />
+            <img src={email_icon} alt=""/>
+            <input type="email" placeholder='Email' />
           </div>
 
           <div className="input">
             <img src={password_icon} alt="" />
-            <input type="password" />
+            <input type="password" placeholder='Password'/>
           </div>
 
         </div>
